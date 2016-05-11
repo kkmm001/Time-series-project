af6433af783ff04b808a6acd9971342d0035cc45
Biwei <biwei.cui@ensae.fr>
2016-05-11 20:00:46 +0200

    data done

--- projet_working.R
+++ projet_working.R
@@ -1,11 +1,11 @@
+#install.packages("forecast", dependencies=TRUE) # dependencies should be loaded
+
+
 library("urca")
 library("tseries")
 library("foreign")
 library("dygraphs")
-<<<<<<< HEAD
 library('forecast')
-=======
->>>>>>> de81c6efb594904161fb2675979083278602ebee
 
 ## Clean up
 rm(list=ls()) 
@@ -14,7 +14,7 @@ cat("\014")
 #Q1 corriger la serie originale -- desaisonnalisation
 
 #Indice brut de la production industrielle (base 100 en 2010) - 
-#Production et distribution d'électricité, de gaz, de vapeur et d'air conditionné (NAF rév. 2, niveau section, poste D)
+#Production et distribution d'?lectricit?, de gaz, de vapeur et d'air conditionn? (NAF r?v. 2, niveau section, poste D)
 
 #i) importation de donnees - GAZ.csv
 data <- read.csv2("Gaz_eletricity.csv", col.names=c("year", "month", "value"))
@@ -22,13 +22,13 @@ ts.data = ts(data$value, frequency = 10, end = c(2016,1),start = c(1990,1))
 plot(ts.data, col=c("blue"))
 
 #ii)acf et pacf disent que ts.data est saisonnier
-ts.acf = acf(ts.data, main="Série brute ACF")
-ts.pacf = pacf(ts.data, main="Série brute PACF")
+ts.acf = acf(ts.data, main="S?rie brute ACF")
+ts.pacf = pacf(ts.data, main="S?rie brute PACF")
 
 #iii) desaisonnalisation par difference saisonniere
 
 ts.diff = diff(ts.data, lag=12)
-plot(ts.diff, main="Série désaisonnalisée")
+plot(ts.diff, main="S?rie d?saisonnalis?e")
 
 #iv) test stationnarite
 pp.test(ts.diff)
@@ -39,18 +39,18 @@ adf.test(ts.diff)
 #Q2 Modele ARMA
 
 #i) En analysant la fn acf/pacf, on retrouve les ordres du modele ARMA tels que:
-acf(ts.diff, main="Série désaisonnalisée")  #q=2
-pacf(ts.diff, main="Série désaisonnalisée") #p=1
+acf(ts.diff, main="S?rie d?saisonnalis?e")  #q=2
+pacf(ts.diff, main="S?rie d?saisonnalis?e") #p=1
 
-# Remarque: on constate que ACF/PACF sont encore significatives. C'est lié à la P/Q du modèle SARIMA
+# Remarque: on constate que ACF/PACF sont encore significatives. C'est li? ? la P/Q du mod?le SARIMA
 
 sarima.ts = arima(ts.diff, order=c(1, 0, 2), seasonal = list(order = c(0, 0, 1), period = 12))
 sarima.ts.1 = arima(ts.diff, order=c(1, 0, 2), seasonal = list(order = c(1, 0, 1), period = 12))
 sarima.ts.2 = arima(ts.diff, order=c(1, 0, 2), seasonal = list(order = c(0, 0, 2), period = 12))
 
-#On en déduit que (P,Q)=(0,1)
+#On en d?duit que (P,Q)=(0,1)
 
-#ii)On calcule la matrice AIC, BIC pour déterminer p,q
+#ii)On calcule la matrice AIC, BIC pour d?terminer p,q
 pmax = 5
 qmax = 5
 
@@ -77,32 +77,33 @@ BICmatrix
 
 # On en trouve un groupe: (p,q)=(2,1)
 
-#iii) Test de significativités pour ces deux groupes
+#iii) Test de significativit?s pour ces deux groupes
 sarima.ts = arima(ts.diff, order=c(2, 0, 1), seasonal = list(order = c(0, 0, 1), period = 12))
 sarima.ts$coef/sqrt(diag(sarima.ts$var.coef)) #pour voir si significatif
 # tous les params sont significatifs.
 
-#iv) Test des résidus
+#iv) Test des r?sidus
 Box.test(sarima.ts$residuals, lag=30,type = c("Box-Pierce"))
 Box.test(sarima.ts$residuals, lag=30,type = c("Ljung-Box"))
 
 tsdiag(sarima.ts, gof.lag=50)
 plot(sarima.ts$residuals)
 
-#v) Test de normalité
+#v) Test de normalit?
 
 qqnorm(sarima.ts$residuals)
 qqline(sarima.ts$residuals)
 
 hist(as.numeric(sarima.ts$residuals))
-probplot(sarima.ts$residuals, qdist=qnorm)
+#probplot(sarima.ts$residuals, qdist=qnorm)
 shapiro.test(sarima.ts$residuals)
 
-<<<<<<< HEAD
+
 #Q3) Prediction
 
 pred = forecast(sarima.ts, h=24, level=95)
 lower = pred$lower
+upper = pred$upper
 
 pred.upper=ts(pred$upper, frequency = 10, end = c(2018,1),start = c(2016,1))
 pred.lower=ts(pred$lower, frequency = 10, end = c(2018,1),start = c(2016,1))
@@ -112,10 +113,8 @@ pred.mean=ts(pred, frequency = 10, end = c(2018,1),start = c(2016,1))
 
 plot(y=ts.diff, x=)
 par(new = TRUE)
-plot(pred, col=c("red"))
+plot(pred$lower, type="o", col=c("red"))
 
 plot()
-=======
 
 
->>>>>>> de81c6efb594904161fb2675979083278602ebee
